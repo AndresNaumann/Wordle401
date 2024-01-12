fa6775276636fc124ebcacfac2cebd35da178fcf
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-01-11 22:51:42 -0700

    works okay, but something is broken

--- sandbox.py
+++ sandbox.py
@@ -19,17 +19,14 @@ def wordle():
         iCount += 1
     print(secretDict)
         
-    #creating a dictionary for the guess word (hard coded, will have to change)
-
-
-    # Fix local variables, may have to not use function or figure a way around it
 
     #creating actions to be performed when the enter key is pressed
     def enter_action(s):
         iCount = 0
         guessDict = {}
         incrament = 0
-        #concatenating the letters in the row
+        numberOfGuesses = 6
+        #concatenating the letters in the row into the guessed work
         finalWord = ""
         for col in range(N_COLS):
             letter = gw.get_square_letter(0, col)
@@ -42,13 +39,14 @@ def wordle():
         if finalWord in FIVE_LETTER_WORDS:
             gw.show_message("Word found")
 
+            # adding each letter to the guessed word dictionary
             for i in finalWord:
                 i = i.upper()
                 guessDict.update({iCount : i})
                 iCount += 1
             print(guessDict)
 
-            
+            # color the letters based on if they're in the secret word or not 
             for i in secretDict:
                 if secretDict[i] == guessDict[i]:
                     gw.set_square_color(incrament, i, CORRECT_COLOR)
@@ -56,19 +54,24 @@ def wordle():
                     gw.set_square_color(incrament, i, PRESENT_COLOR)
                 else:
                     gw.set_square_color(incrament, i, MISSING_COLOR)
-                
+
+            # check if the player has won or lost
+            # might have to swith the order of the if statements
+            if secretDict == guessDict:
+                gw.show_message("You win!")
+                gw.exit_on_click()
+            elif numberOfGuesses == 0:
+                gw.show_message("You lose!")
+                gw.exit_on_click()
+            else:
+                gw.set_current_row(gw.get_current_row() + 1)
+                numberOfGuesses -= 1
+
+
+
+        # if the input word is in the word list:         
         else:
             gw.show_message("Not in word list")
-
-    #checking if the guess letters is in the secret word, one letter at a time
-    # incrament = 0
-    # for i in secretDict:
-    #     if secretDict[i] == guessDict[i]:
-    #         print(f'{i} , {incrament}')
-    #     elif secretDict[i] in guessDict.values():
-    #         print("in word")
-    #     else:
-    #         print("not in word")
 
     gw.add_enter_listener(enter_action)
     
