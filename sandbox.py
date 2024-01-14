96960b734b583a6402e1a76db0a94484c30d55c3
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-01-13 18:22:12 -0700

    double letters and new logic (working)

--- sandbox.py
+++ sandbox.py
@@ -7,66 +7,83 @@ def wordle():
     # load graphical interface
     gw = WordleGWindow()
 
-    # initialize variables, the secret word, and the guess word
-    secretDict = {}
-
     # selecting a secret word from five letter words and converting to uppercase
-    # secret_word = random.choice(FIVE_LETTER_WORDS).upper()
+    # secretWord = random.choice(FIVE_LETTER_WORDS).upper()
 
-    # testing double letter guess DOORS
-    secret_word = "VODKA"
+    # # testing double letter guess DOORS
+    secretWord = "STEAL"
 
-    # creating a dictionary for the secret word
-    iCount = 0
-    for i in secret_word:
-        secretDict.update({iCount: i})
-        iCount += 1
-    print(secretDict)
+    #function to create a list of individual letter in a word
+    def makeWordList(word):
+        wordList = []
+        wordList = list(word)
+        return wordList
+
+    testingList = makeWordList(secretWord)
+    print(testingList)
+    
+
 
     # creating actions to be performed when the enter key is pressed
     def enter_action(s):
+
+
+        def makeWordList(word):
+            word = word.upper()
+            wordList = []
+            wordList = list(word)
+            return wordList
+        
+        changingList = makeWordList(secretWord)
+        print(changingList)
+    
         # initializing variables
-        iCount = 0
-        guessDict = {}
-        incrament = 0
         numberOfGuesses = 6
+        fillerVar = '*'
 
         # concatenating the letters in the row into the guessed word
-        finalWord = ""
+        guessWord = ""
         for col in range(N_COLS):
             letter = gw.get_square_letter(gw.get_current_row(), col)
-            finalWord += letter
+            guessWord += letter
         # checking if the guess word is in the dictionary
-        finalWord = finalWord.lower()
-        print(finalWord)
+        guessWord = guessWord.lower()
+        print(guessWord)
 
         # checking if the guess word is in the dictionary
-        if finalWord in FIVE_LETTER_WORDS:
+        if guessWord in FIVE_LETTER_WORDS:
             gw.show_message("Word found")
 
-            # adding each letter to the guessed word dictionary
-            for i in finalWord:
-                i = i.upper()
-                guessDict.update({iCount: i})
-                iCount += 1
-            print(guessDict)
-            print(guessDict.values())
+            #makes a list of letters in the guessed word
+            guessList = makeWordList(guessWord)
+            print(guessList)
 
             # color the letters based on if they're in the secret word or not
-            for i in guessDict:
-                if guessDict[i] == secretDict[i]:
+            for i in range(len(guessList)):
+                if guessList[i] == testingList[i]:
                     gw.set_square_color(gw.get_current_row(), i, CORRECT_COLOR)
+                    changingList[i] = fillerVar
                     print("correct")
-                elif guessDict[i] in secretDict.values():
+                elif guessList[i] in changingList:
                     gw.set_square_color(gw.get_current_row(), i, PRESENT_COLOR)
-                    print("present")
+                    
+                    for e in range(len(testingList)):
+                        if guessList[i] == changingList[e]:
+                            changingList[e] = fillerVar
+                            break
+                    guessList[i] = fillerVar
+                    
                 else:
                     gw.set_square_color(gw.get_current_row(), i, MISSING_COLOR)
                     print("missing")
-
+            print(changingList)
+            print(guessList)
+            print(testingList)
+         
+            
             # check if the player has won or lost
             # might have to swith the order of the if statements
-            if secretDict == guessDict:
+            if testingList == guessList:
                 gw.show_message("You win!")
 
             elif numberOfGuesses == 0:
@@ -83,6 +100,11 @@ def wordle():
             clear_current_row(gw)
             gw.set_current_row(gw.get_current_row())
 
+        #reset the changing list between submissions
+        changingList = makeWordList(secretWord)
+
+
+    
     gw.add_enter_listener(enter_action)
 
 
@@ -92,3 +114,9 @@ def clear_current_row(gw):
 
 
 wordle()
+
+# to-do-list:
+# stop game after win or loss
+# only certian number of plays 
+# clear row on no word found
+
