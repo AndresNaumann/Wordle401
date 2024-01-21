14273ba3ba6b34750d46df5437d11ddedecfa78c
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-01-20 19:56:25 -0700

    wordle fixes

--- sandbox.py
+++ sandbox.py
@@ -29,88 +29,82 @@ def wordle():
     # creating actions to be performed when the enter key is pressed
     def enter_action(s):
 
-        if gameLength == 0:
-            gw.show_message("You lose!")
-            return
+        
+        #function to create a list of individual letter in a word
+        def makeWordList(word):
+            word = word.upper()
+            wordList = []
+            wordList = list(word)
+            return wordList
 
-        else:
-
-
-            def makeWordList(word):
-                word = word.upper()
-                wordList = []
-                wordList = list(word)
-                return wordList
-
-            changingList = makeWordList(secretWord)
-            print(changingList)
+        changingList = makeWordList(secretWord)
+        print(changingList)
 
             # initializing variables
-            numberOfGuesses = 6
-            fillerVar = '*'
+          
+        fillerVar = '*'
 
             # concatenating the letters in the row into the guessed word
-            guessWord = ""
-            for col in range(N_COLS):
-                letter = gw.get_square_letter(gw.get_current_row(), col)
-                guessWord += letter
+        guessWord = ""
+        for col in range(N_COLS):
+            letter = gw.get_square_letter(gw.get_current_row(), col)
+            guessWord += letter
             # checking if the guess word is in the dictionary
-            guessWord = guessWord.lower()
-            print(guessWord)
+        guessWord = guessWord.lower()
+        # print(guessWord)
 
             # checking if the guess word is in the dictionary
-            if guessWord in FIVE_LETTER_WORDS:
-                gw.show_message("Word found")
-
-                #makes a list of letters in the guessed word
-                guessList = makeWordList(guessWord)
-                print(guessList)
-
-                # color the letters based on if they're in the secret word or not
-                for i in range(len(guessList)):
-                    if guessList[i] == testingList[i]:
-                        gw.set_square_color(gw.get_current_row(), i, CORRECT_COLOR)
-                        changingList[i] = fillerVar
-                        print("correct")
-                    elif guessList[i] in changingList:
-                        gw.set_square_color(gw.get_current_row(), i, PRESENT_COLOR)
-
-                        for e in range(len(testingList)):
-                            if guessList[i] == changingList[e]:
-                                changingList[e] = fillerVar
-                                break
-                        guessList[i] = fillerVar
+        if guessWord in FIVE_LETTER_WORDS:
+            gw.show_message("Word found")
+            #makes a list of letters in the guessed word
+            guessList = makeWordList(guessWord)
+            # print(guessList)
+            # color the letters based on if they're in the secret word or not
+            for i in range(len(guessList)):
+                if guessList[i] == testingList[i]:
+                    gw.set_square_color(gw.get_current_row(), i, CORRECT_COLOR)
+                    changingList[i] = fillerVar
+                    # print("correct")
+                elif guessList[i] in changingList:
+                    gw.set_square_color(gw.get_current_row(), i, PRESENT_COLOR)
+                    for e in range(len(testingList)):
+                        if guessList[i] == changingList[e]:
+                            changingList[e] = fillerVar
+                            break
+                    guessList[i] = fillerVar
                     
-                    else:
-                        gw.set_square_color(gw.get_current_row(), i, MISSING_COLOR)
-                        print("missing")
-                print(changingList)
-                print(guessList)
-                print(testingList)
-
-            
-                # check if the player has won or lost
-                # might have to swith the order of the if statements
-                if testingList == guessList:
-                    gw.show_message("You win!")
-                    return
-
-                elif gameLength == 1:
-                    gw.show_message("You lose!")
-                    
-
                 else:
-                    gw.set_current_row(gw.get_current_row() + 1)
-                    gameLength -= 1
+                    gw.set_square_color(gw.get_current_row(), i, MISSING_COLOR)
+                    print("missing")
+            # print(changingList)
+            # print(guessList)
+            # print(testingList)
+        
+            # check if the player has won or lost
+            # might have to swith the order of the if statements
+            turnsleft = (N_ROWS - gw.get_current_row()) - 1
+            print(turnsleft)
 
-            # if the input word is in the word list:
+            if testingList == guessList:
+                gw.show_message("You win!")
+                
+            
+            elif turnsleft == 0:
+                gw.show_message("You lose!")
+                
+        
             else:
-                gw.show_message("Not in word list")
-                clear_current_row(gw)
-                gw.set_current_row(gw.get_current_row())
+                gw.set_current_row(gw.get_current_row() + 1)
+                
 
-            #reset the changing list between submissions
-            changingList = makeWordList(secretWord)
+        # if the input word is in the word list:
+        else:
+            gw.show_message("Not in word list")
+            clear_current_row(gw)
+            gw.set_current_row(gw.get_current_row())
+
+       #reset the changing list between submissions
+        changingList = makeWordList(secretWord)
 
 
     
